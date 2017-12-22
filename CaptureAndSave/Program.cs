7d7e9d9a2155bb13c92ec4512e479eb4acbaf815
Tarasovych <tarasovych.ua@gmail.com>
2017-12-22 23:05:21 +0200

    Initial commit

--- CaptureAndSave/Program.cs
+++ CaptureAndSave/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Threading;
 
 namespace CaptureAndSave
 {
@@ -10,6 +9,25 @@ namespace CaptureAndSave
     {
         static void Main(string[] args)
         {
+            ConsoleKeyInfo cki;
+            while (true)
+            {
+                cki = Console.ReadKey(true);
+                if (cki.Key == ConsoleKey.Escape)
+                    break;
+                if (cki.Key == ConsoleKey.S)
+                    using (Bitmap bitmap = new Bitmap(1920, 1080))
+                    {
+                        using (Graphics g = Graphics.FromImage(bitmap))
+                        {
+                            g.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+                        }
+                        Console.WriteLine("saving the image...");
+                        string name = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+                        bitmap.Save(AppDomain.CurrentDomain.BaseDirectory + "\\" + name + ".jpg", ImageFormat.Jpeg);
+                        Console.WriteLine(name + ".jpg has been saved");
+                    }
+            }
         }
     }
 }
